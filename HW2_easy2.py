e1a81676bbcc5656521ac7248c792cdcd2339a65
AssiaAzov <assia.azov@gmail.com>
2018-08-11 22:17:53 +0400

    исправила 2 задачу easy, работает правильно

--- HW2_easy2.py
+++ HW2_easy2.py
@@ -1,38 +1,20 @@
-list = [1, 2, 2, 3, 4, 4, 5, 2, 5, 6, 7, 8]
+# Задача-2:
+# Даны два произвольные списка.
+# Удалите из первого списка элементы, присутствующие во втором списке.
+
+list1 = [1, 2, 2, 3, 4, 4, 5, 2, 5, 6, 7, 8]
 list2 = [2, 4, 6, 6, 8]
 i = 0
 j = 0
 
-while i < len(list2):
-    while j < len(list):
-        if list2[i] == list[j]:
-            list.remove(list[j])
-            print(list)
-        else:
-            j += 1
-            print(list)
+while i < len(list1):
+    if list2.count(list1[i]) > 0:
+        list1.remove(list1[i])
     else:
         i += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print(list)
+print(list1)
 
 
 
