94e2f4654548eed71a2694bb88191ce7b9883ba1
AssiaAzov <assia.azov@gmail.com>
2018-08-19 00:31:05 +0400

    удалила домашку 3 урока из ветки 4 урока

--- HW3_easy2.py
+++
@@ -1,8 +0,0 @@
-# Задание - 2
-# Создайте функцию, принимающую на вход 3 числа, и возвращающую наибольшее из них
-
-def max_count(a, b, c):
-    return max(a, b, c)
-
-num = tuple(map(int, input('Введите 3 числа через пробел: ').split()))
-print(max_count(num[0], num[1], num[2]))
